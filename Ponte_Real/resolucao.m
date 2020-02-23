b5d3180afba779c475c3d0dd7a70e72cc7c63f72
jorgeconti <jorgeconti@users.noreply.github.com>
2020-02-23 14:14:28 -0300

    ajustes
    criado repartição para ponte real

---
+++ Ponte_Real/resolucao.m
@@ -0,0 +1,14 @@
+tic;
+disp("Carregando arquivos");
+aux1E=aux1E(1:reducao:end,1:reducao:1280);disp("8 aux1E "+toc+ "Seg");tic
+aux1X=aux1X(1:reducao:end,1:reducao:1280);disp("7 aux1X "+toc+ "Seg");tic;
+aux1Z=aux1Z(1:reducao:end,1:reducao:1280);disp("6 aux1Z "+toc+ "Seg");tic;
+aux1I=aux1I(1:reducao:end,1:reducao:1280);disp("5.5 aux1I "+toc+ "Seg");tic;
+aux1T=aux1T(1:reducao:end,:);disp("5 aux1T "+toc+ "Seg");tic;
+aux2E=aux2E(1:reducao:end,1:reducao:1280);disp("4 aux2E "+toc+ "Seg");tic;
+aux2X=aux2X(1:reducao:end,1:reducao:1280);disp("3 aux2X "+toc+ "Seg");tic;
+aux2Z=aux2Z(1:reducao:end,1:reducao:1280);disp("2 aux2Z "+toc+ "Seg");tic;
+aux2I=aux2I(1:reducao:end,1:reducao:1280);disp("1.5 aux2I "+toc+ "Seg");tic;
+aux2T=aux2T(1:reducao:end,:);disp("1 aux2T "+toc+ "Seg");tic;
+disp("Arquivos carregados");
+
