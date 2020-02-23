d3f1f2d2824d23d6e3b908d8b3ea8c9a7a4fdd10
jorgeconti <jorgeconti@users.noreply.github.com>
2020-02-23 14:27:25 -0300

    Finalizado
    ponte real finalizada

--- Ponte_Real/resolucao.m
+++ Ponte_Real/resolucao.m
@@ -1,14 +1,14 @@
 tic;
 disp("Carregando arquivos");
-aux1E=aux1E(1:reducao:end,1:reducao:1280);disp("8 aux1E "+toc+ "Seg");tic
-aux1X=aux1X(1:reducao:end,1:reducao:1280);disp("7 aux1X "+toc+ "Seg");tic;
-aux1Z=aux1Z(1:reducao:end,1:reducao:1280);disp("6 aux1Z "+toc+ "Seg");tic;
-aux1I=aux1I(1:reducao:end,1:reducao:1280);disp("5.5 aux1I "+toc+ "Seg");tic;
-aux1T=aux1T(1:reducao:end,:);disp("5 aux1T "+toc+ "Seg");tic;
-aux2E=aux2E(1:reducao:end,1:reducao:1280);disp("4 aux2E "+toc+ "Seg");tic;
-aux2X=aux2X(1:reducao:end,1:reducao:1280);disp("3 aux2X "+toc+ "Seg");tic;
-aux2Z=aux2Z(1:reducao:end,1:reducao:1280);disp("2 aux2Z "+toc+ "Seg");tic;
-aux2I=aux2I(1:reducao:end,1:reducao:1280);disp("1.5 aux2I "+toc+ "Seg");tic;
-aux2T=aux2T(1:reducao:end,:);disp("1 aux2T "+toc+ "Seg");tic;
+laser1Encoder=laser1Encoder(1:reducao:end,1:reducao:1280);disp("8 laser1Encoder "+toc+ "Seg");tic
+laser1X=laser1X(1:reducao:end,1:reducao:1280);disp("7 laser1X "+toc+ "Seg");tic;
+laser1Z=laser1Z(1:reducao:end,1:reducao:1280);disp("6 laser1Z "+toc+ "Seg");tic;
+laser1Intensidade=laser1Intensidade(1:reducao:end,1:reducao:1280);disp("5.5 laser1Intensidade "+toc+ "Seg");tic;
+laser1Amostragem=laser1Amostragem(1:reducao:end,:);disp("5 laser1Amostragem "+toc+ "Seg");tic;
+laser2Encoder=laser2Encoder(1:reducao:end,1:reducao:1280);disp("4 laser2Encoder "+toc+ "Seg");tic;
+laser2X=laser2X(1:reducao:end,1:reducao:1280);disp("3 laser2X "+toc+ "Seg");tic;
+laser2Z=laser2Z(1:reducao:end,1:reducao:1280);disp("2 laser2Z "+toc+ "Seg");tic;
+laser2Intensidade=laser2Intensidade(1:reducao:end,1:reducao:1280);disp("1.5 laser2Intensidade "+toc+ "Seg");tic;
+laser2Amostragem=laser2Amostragem(1:reducao:end,:);disp("1 laser2Amostragem "+toc+ "Seg");tic;
 disp("Arquivos carregados");
 
